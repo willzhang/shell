8cacf69ee12329043a77f8f05f4b33acce525750
will <willzhmic@outlook.com>
2020-12-19 10:19:56 +0800

    Update disalbe-firewalld.sh

--- disalbe-firewalld.sh
+++ disalbe-firewalld.sh
@@ -1,15 +1,18 @@
 #关闭firewall防火墙
+
 ```
 systemctl disable --now firewalld
 ```
 
 #关闭selinux
+
 ```
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 ```
 
 #配置时间同步
+
 ```
 yum install -y chrony
 systemctl enable --now chronyd
