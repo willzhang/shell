b31e11dda41cf5302519e9427395ecd855aa950d
will <willzhmic@outlook.com>
2020-12-19 10:20:22 +0800

    Update disalbe-firewalld.sh

--- disalbe-firewalld.sh
+++ disalbe-firewalld.sh
@@ -17,3 +17,15 @@ sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 yum install -y chrony
 systemctl enable --now chronyd
 ```
+
+
+### Install latest version of dokcer-ce in centos7 or ubuntu
+```
+# install docker
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl enable --now docker
+
+# install docker with huaweicloud registry mirror
+curl -fsSL https://raw.githubusercontent.com/willzhang/shell/master/docker-ce.sh | sh
+
+# install docker-compose
+curl -fs
